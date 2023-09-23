d179b674d2dc7f6b3069624806b92a422c43043e
Raehyeon Jeong <raehy19@gmail.com>
2023-09-24 03:41:39 +0900

    feat : bitNor

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -30,24 +30,24 @@ INTEGER CODING RULES:
   must conform to the following style:
  
   int Funct(arg1, arg2, ...) {
-      /* brief description of how your implementation works */
-      int var1 = Expr1;
-      ...
-      int varM = ExprM;
+	  /* brief description of how your implementation works */
+	  int var1 = Expr1;
+	  ...
+	  int varM = ExprM;
 
-      varJ = ExprJ;
-      ...
-      varN = ExprN;
-      return ExprR;
+	  varJ = ExprJ;
+	  ...
+	  varN = ExprN;
+	  return ExprR;
   }
 
   Each "Expr" is an expression using ONLY the following:
   1. Integer constants 0 through 255 (0xFF), inclusive. You are
-      not allowed to use big constants such as 0xffffffff.
+	  not allowed to use big constants such as 0xffffffff.
   2. Function arguments and local variables (no global variables).
   3. Unary integer operations ! ~
   4. Binary integer operations & ^ | + << >>
-    
+
   Some of the problems restrict the set of allowed operators even further.
   Each "Expr" may consist of multiple operators. You are not restricted to
   one operator per line.
@@ -60,14 +60,14 @@ INTEGER CODING RULES:
   5. Use any other operations, such as &&, ||, -, or ?:
   6. Use any form of casting.
   7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
+	 cannot use arrays, structs, or unions.
 
- 
+
   You may assume that your machine:
   1. Uses 2s complement, 32-bit representations of integers.
   2. Performs right shifts arithmetically.
   3. Has unpredictable behavior when shifting if the shift amount
-     is less than 0 or greater than 31.
+	 is less than 0 or greater than 31.
 
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
@@ -75,18 +75,18 @@ EXAMPLES OF ACCEPTABLE CODING STYLE:
    * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
    */
   int pow2plus1(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     return (1 << x) + 1;
+	 /* exploit ability of shifts to compute powers of 2 */
+	 return (1 << x) + 1;
   }
 
   /*
    * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
    */
   int pow2plus4(int x) {
-     /* exploit ability of shifts to compute powers of 2 */
-     int result = (1 << x);
-     result += 4;
-     return result;
+	 /* exploit ability of shifts to compute powers of 2 */
+	 int result = (1 << x);
+	 result += 4;
+	 return result;
   }
 
 FLOATING POINT CODING RULES
@@ -103,24 +103,24 @@ You are expressly forbidden to:
   3. Call any functions.
   4. Use any form of casting.
   5. Use any data type other than int or unsigned.  This means that you
-     cannot use arrays, structs, or unions.
+	 cannot use arrays, structs, or unions.
   6. Use any floating point data types, operations, or constants.
 
 
 NOTES:
-  1. Use the dlc (data lab checker) compiler (described in the handout) to 
-     check the legality of your solutions.
+  1. Use the dlc (data lab checker) compiler (described in the handout) to
+	 check the legality of your solutions.
   2. Each function has a maximum number of operations (integer, logical,
-     or comparison) that you are allowed to use for your implementation
-     of the function.  The max operator count is checked by dlc.
-     Note that assignment ('=') is not counted; you may use as many of
-     these as you want without penalty.
+	 or comparison) that you are allowed to use for your implementation
+	 of the function.  The max operator count is checked by dlc.
+	 Note that assignment ('=') is not counted; you may use as many of
+	 these as you want without penalty.
   3. Use the btest test harness to check your functions for correctness.
   4. Use the BDD checker to formally verify your functions
   5. The maximum number of ops for each function is given in the
-     header comment for each function. If there are any inconsistencies 
-     between the maximum ops in the writeup and in this file, consider
-     this file the authoritative source.
+	 header comment for each function. If there are any inconsistencies
+	 between the maximum ops in the writeup and in this file, consider
+	 this file the authoritative source.
 
 /*
  * STEP 2: Modify the following functions according the coding rules.
@@ -169,6 +169,7 @@ NOTES:
    - 56 emoji characters
    - 285 hentaigana
    - 3 additional Zanabazar Square characters */
+
 /* 
  * bitNor - ~(x|y) using only ~ and & 
  *   Example: bitNor(0x6, 0x5) = 0xFFFFFFF8
@@ -177,9 +178,11 @@ NOTES:
  *   Rating: 1
  */
 int bitNor(int x, int y) {
-  return 2;
+//	(~(x | y)) == (~x & ~y)
+	return (~x & ~y);
 }
-/* 
+
+/*
  * getByte - Extract byte n from word x
  *   Bytes numbered from 0 (least significant) to 3 (most significant)
  *   Examples: getByte(0x12345678,1) = 0x56
@@ -188,9 +191,10 @@ int bitNor(int x, int y) {
  *   Rating: 2
  */
 int getByte(int x, int n) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * bitMask - Generate a mask consisting of all 1's 
  *   lowbit and highbit
  *   Examples: bitMask(5,3) = 0x38
@@ -201,9 +205,10 @@ int getByte(int x, int n) {
  *   Rating: 3
  */
 int bitMask(int highbit, int lowbit) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * bang - Compute !x without using !
  *   Examples: bang(3) = 0, bang(0) = 1
  *   Legal ops: ~ & ^ | + << >>
@@ -211,8 +216,9 @@ int bitMask(int highbit, int lowbit) {
  *   Rating: 4 
  */
 int bang(int x) {
-  return 2;
+	return 2;
 }
+
 /*
  * bitParity - returns 1 if x contains an odd number of 0's
  *   Examples: bitParity(5) = 0, bitParity(7) = 1
@@ -221,18 +227,20 @@ int bang(int x) {
  *   Rating: 4
  */
 int bitParity(int x) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * TMax - return maximum two's complement integer 
  *   Legal ops: ! ~ & ^ | + << >>
  *   Max ops: 4
  *   Rating: 1
  */
 int tmax(void) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * isNegative - return 1 if x < 0, return 0 otherwise 
  *   Example: isNegative(-1) = 1.
  *   Legal ops: ! ~ & ^ | + << >>
@@ -240,9 +248,10 @@ int tmax(void) {
  *   Rating: 2
  */
 int isNegative(int x) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * fitsBits - return 1 if x can be represented as an 
  *  n-bit, two's complement integer.
  *   1 <= n <= 32
@@ -252,9 +261,10 @@ int isNegative(int x) {
  *   Rating: 2
  */
 int fitsBits(int x, int n) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * dividePower2 - Compute x/(2^n), for 0 <= n <= 30
  *  Round toward zero
  *   Examples: dividePower2(15,1) = 7, dividePower2(-33,4) = -2
@@ -263,9 +273,10 @@ int fitsBits(int x, int n) {
  *   Rating: 2
  */
 int dividePower2(int x, int n) {
-    return 2;
+	return 2;
 }
-/* 
+
+/*
  * conditional - same as x ? y : z 
  *   Example: conditional(2,4,5) = 4
  *   Legal ops: ! ~ & ^ | + << >>
@@ -273,8 +284,9 @@ int dividePower2(int x, int n) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+	return 2;
 }
+
 /*
  * ezThreeFourths - multiplies by 3/4 rounding toward 0,
  *   Should exactly duplicate effect of C expression (x*3/4),
@@ -287,9 +299,10 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int ezThreeFourths(int x) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * signMag2TwosComp - Convert from sign-magnitude to two's complement
  *   where the MSB is the sign bit
  *   Example: signMag2TwosComp(0x80000005) = -5.
@@ -298,9 +311,10 @@ int ezThreeFourths(int x) {
  *   Rating: 4
  */
 int signMag2TwosComp(int x) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * floatAbsVal - Return bit-level equivalent of absolute value of f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
@@ -312,9 +326,10 @@ int signMag2TwosComp(int x) {
  *   Rating: 2
  */
 unsigned floatAbsVal(unsigned uf) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * floatFloat2Int - Return bit-level equivalent of expression (int) f
  *   for floating point argument f.
  *   Argument is passed as unsigned int, but
@@ -327,9 +342,10 @@ unsigned floatAbsVal(unsigned uf) {
  *   Rating: 4
  */
 int floatFloat2Int(unsigned uf) {
-  return 2;
+	return 2;
 }
-/* 
+
+/*
  * floatScale4 - Return bit-level equivalent of expression 4*f for
  *   floating point argument f.
  *   Both the argument and result are passed as unsigned int's, but
@@ -341,5 +357,5 @@ int floatFloat2Int(unsigned uf) {
  *   Rating: 4
  */
 unsigned floatScale4(unsigned uf) {
-    return 2;
+	return 2;
 }
