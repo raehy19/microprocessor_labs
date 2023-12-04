d26a8b4ed4d340a725a3c9aaff0326270c4e11d9
Raehyeon Jeong <raehy19@gmail.com>
2023-12-04 20:14:21 +0900

    feat : shell lab : handle signal, run command foreground and background method

--- shell_lab/tsh.c
+++ shell_lab/tsh.c
@@ -3,6 +3,7 @@
  * 
  * Name       : Raehyeon Jeong
  * Student ID : 2020142218
+ * Date       : 2023 Dec 04
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,7 +27,7 @@
 #define BG 2    /* running in background */
 #define ST 3    /* stopped */
 
-/* 
+/*
  * Jobs states: FG (foreground), BG (background), ST (stopped)
  * Job state transitions and enabling actions:
  *     FG -> ST  : ctrl-z
@@ -106,7 +107,7 @@ typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
 
 /*
- * main - The shell's main routine 
+ * main - The shell's main routine
  */
 int main(int argc, char **argv) {
 	char c;
@@ -171,21 +172,24 @@ int main(int argc, char **argv) {
 	exit(0); /* control never reaches here */
 }
 
-/* 
+/*
  * eval - Evaluate the command line that the user has just typed in
- * 
+ *
  * If the user has requested a built-in command (quit, jobs, bg or fg)
  * then execute it immediately. Otherwise, fork a child process and
  * run the job in the context of the child. If the job is running in
  * the foreground, wait for it to terminate and then return.  Note:
  * each child process must have a unique process group ID so that our
  * background children don't receive SIGINT (SIGTSTP) from the kernel
- * when we type ctrl-c (ctrl-z) at the keyboard.  
+ * when we type ctrl-c (ctrl-z) at the keyboard.
 */
 void eval(char *cmdline) {
-	char *argv[MAXARGS];  // char ** type variable : to store input command
-	int is_bg;            // int type variable     : to check if command should run background
-	pid_t pid;            // pid_t type variable   : to store pid value from fork() function call
+	char *argv[MAXARGS]; // char ** type variable  : to store input command
+	int is_bg;           // int type variable      : to check if command should run background
+	pid_t pid;           // pid_t type variable    : to store pid value from fork() function call
+	sigset_t set_full;   // sigset_t type variable : filled set to block all signal
+	sigset_t old_set;    // sigset_t type variable : old sigset to restore sigset
+	struct job_t *job;   // job_t type variable    : to print info when executed background
 
 	// Parse cmdline and check if command should run background
 	is_bg = parseline(cmdline, argv);
@@ -195,47 +199,82 @@ void eval(char *cmdline) {
 		return;
 	}
 
-//	// Debug print
-//	printf("%d\n", is_bg);
-//	for (int i = 0; *(argv + i); i++) {
-//		printf("%s\n", *(argv + i));
-//	}
-
-	// Execute and return if command is builtin function
+	// Execute and return if command is built-in function
 	if (builtin_cmd(argv))
 		return;
 
-	// Execute command background
-	if (is_bg) {
-
-		return;
+	// Block signal to avoid race condition on job list
+	sigfillset(&set_full);
+	if (sigprocmask(SIG_BLOCK, &set_full, &old_set)) {
+		printf("eval : sigprocmask block all failed\n");
 	}
 
-	// Execute command foreground
+	// If it is not built-in function, execute command
 	// Child process
 	if ((pid = fork()) == 0) {
+
+		// Call setpgid
+		if (setpgid(0, 0)) {
+			printf("eval : setpgid failed\n");
+		}
+
+		// Restore signal set
+		if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+			printf("eval : sigprocmask restore failed\n");
+		}
+
 		// execute command
 		if (execve(argv[0], argv, environ) < 0) {
+
+			// If fail, print and exit
 			printf("%s : Command not found\n", argv[0]);
 			exit(0);
 		}
 	}
 		// Parent process
 	else if (pid > 0) {
-		int status;
+		// If command should run background
+		if (is_bg) {
 
-		waitpid(pid, &status, 0);
-	} else { ;
+			// Add job to job list
+			addjob(jobs, pid, BG, cmdline);
+
+			// Restore signal set
+			if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+				printf("eval : sigprocmask restore failed\n");
+			}
+
+			// Print background process info
+			job = getjobpid(jobs, pid);
+			printf("[%d] (%d) %s", job->jid, job->pid, job->cmdline);
+		}
+			// If command should run foreground
+		else {
+
+			// Add job to job list
+			addjob(jobs, pid, FG, cmdline);
+
+			// Restore signal set
+			if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+				printf("eval : sigprocmask restore failed\n");
+			}
+
+			// Wait until child process ends
+			waitfg(pid);
+		}
+	} // If fork() failed
+	else {
+		printf("fork failed\n");
 	}
 	return;
 }
 
-/* 
+/*
  * parseline - Parse the command line and build the argv array.
- * 
+ *
  * Characters enclosed in single quotes are treated as a single
  * argument.  Return true if the user has requested a BG job, false if
- * the user has requested a FG job.  
+ * the user has requested a FG job.
  */
 int parseline(const char *cmdline, char **argv) {
 	static char array[MAXLINE]; /* holds local copy of command line */
@@ -284,33 +323,50 @@ int parseline(const char *cmdline, char **argv) {
 	return bg;
 }
 
-/* 
+/*
  * builtin_cmd - If the user has typed a built-in command then execute
- *    it immediately.  
+ *    it immediately.
  */
 int builtin_cmd(char **argv) {
-	// Check if there is only one command argument
-	if (argv[0] && !argv[1]) {
-		// Execute built-in function : exit
-		if (!strncmp(argv[0], "quit", 4)) {
-			printf("quit!!!!!!\n");
-			exit(0);
-		}
+	// Execute built-in function : exit
+	if (!strncmp(argv[0], "quit", 4) && !argv[1]) {
+		exit(0);
 	}
-	return 0;     /* not a builtin command */
+	// Execute built-in function : jobs
+	if (!strncmp(argv[0], "jobs", 4) && !argv[1]) {
+		listjobs(jobs);
+		return (1);
+	}
+	// Execute built-in function : fg and bg
+	if (!strncmp(argv[0], "fg", 2) || !strncmp(argv[0], "bg", 2)) {
+		do_bgfg(argv);
+		return (1);
+	}
+	return (0);     /* not a builtin command */
 }
 
-/* 
+/*
  * do_bgfg - Execute the builtin bg and fg commands
  */
 void do_bgfg(char **argv) {
+	// Todo : bgfg
+	printf("fgbg\n");
 	return;
 }
 
-/* 
+/*
  * waitfg - Block until process pid is no longer the foreground process
  */
 void waitfg(pid_t pid) {
+	struct job_t *job;
+
+	job = getjobpid(jobs, pid);
+
+	// Use busy waiting loop
+	while (job->state == FG) {
+		usleep(1000);
+	}
+
 	return;
 }
 
@@ -318,32 +374,177 @@ void waitfg(pid_t pid) {
  * Signal handlers
  *****************/
 
-/* 
+/*
  * sigchld_handler - The kernel sends a SIGCHLD to the shell whenever
  *     a child job terminates (becomes a zombie), or stops because it
  *     received a SIGSTOP or SIGTSTP signal. The handler reaps all
  *     available zombie children, but doesn't wait for any other
- *     currently running children to terminate.  
+ *     currently running children to terminate.
  */
 void sigchld_handler(int sig) {
+	sigset_t set_full;   // sigset_t type variable : filled set to block all signal
+	sigset_t old_set;    // sigset_t type variable : old sigset to restore sigset
+	int old_errno;       // int type variable      : store errno to restore
+	int status;          // int type variable      : to check process status
+	pid_t pid;           // pid_t type variable    : to store pid value from fork() function call
+	struct job_t *job;   // job_t type variable    : to delete or stop, print info
+
+	// Set full sigset
+	sigfillset(&set_full);
+
+	// Backup errno
+	old_errno = errno;
+
+	while ((pid = waitpid(-1, &status, WNOHANG | WUNTRACED)) > 0) {
+		// If process terminated
+		if (WIFEXITED(status) || WIFSIGNALED(status)) {
+
+			job = getjobpid(jobs, pid);
+
+			// If received SIGINT
+			if (WIFSIGNALED(status) != 0) {
+				printf("Job [%d] (%d) terminated by signal 2\n", job->jid, job->pid);
+			}
+
+			// Block signal to avoid race condition on job list
+			if (sigprocmask(SIG_BLOCK, &set_full, &old_set)) {
+				printf("sigchld handler : sigprocmask block all failed\n");
+			}
+
+			// Delete job
+			deletejob(jobs, pid);
+
+			// Restore signal
+			if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+				printf("sigchld handler : sigprocmask restore sigset failed\n");
+			}
+
+		}
+			// If process stopped
+		else if (WIFSTOPPED(status) != 0) {
+
+			job = getjobpid(jobs, pid);
+
+			// If received SIGTSTP
+			printf("Job [%d] (%d) stopped by signal 20\n", job->jid, job->pid);
+
+			// Block signal to avoid race condition on job list
+			if (sigprocmask(SIG_BLOCK, &set_full, &old_set)) {
+				printf("sigchld handler : sigprocmask block all failed\n");
+			}
+
+			// Stop job
+			job->state = ST;
+
+			// Restore signal
+			if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+				printf("sigchld handler : sigprocmask restore sigset failed\n");
+			}
+
+		}
+	}
+
+	// Restore errno
+	errno = old_errno;
+
 	return;
 }
 
-/* 
+/*
  * sigint_handler - The kernel sends a SIGINT to the shell whenver the
  *    user types ctrl-c at the keyboard.  Catch it and send it along
- *    to the foreground job.  
+ *    to the foreground job.
  */
 void sigint_handler(int sig) {
+	sigset_t set;
+	sigset_t old_set;
+	int old_errno;
+	pid_t pid;
+
+	// Empty sigset
+	if (sigemptyset(&set)) {
+		printf("sigint handler : sigemptyset failed\n");
+	}
+
+	// Add SIGINT to sigset
+	if (sigaddset(&set, SIGINT)) {
+		printf("sigint handler : sigemptyset failed\n");
+	}
+
+	// Backup errno
+	old_errno = errno;
+
+	// If current foreground job exist
+	if ((pid = fgpid(jobs))) {
+
+		// Block SIGINT to prevent unexpected behavior
+		if (sigprocmask(SIG_BLOCK, &set, &old_set)) {
+			printf("sigint handler : sigprocmask block SIGINT failed\n");
+		}
+
+		// Send SIGINT signal to job
+		if (kill(pid, SIGINT)) {
+			printf("sigint handler : kill SIGINT to pid %d failed\n", pid);
+		}
+
+		// Restore sigset prevent unexpected behavior
+		if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+			printf("sigint handler : sigprocmask restore sigset failed\n");
+		}
+	}
+
+	// Restore errno
+	errno = old_errno;
+
 	return;
 }
 
 /*
  * sigtstp_handler - The kernel sends a SIGTSTP to the shell whenever
  *     the user types ctrl-z at the keyboard. Catch it and suspend the
- *     foreground job by sending it a SIGTSTP.  
+ *     foreground job by sending it a SIGTSTP.
  */
 void sigtstp_handler(int sig) {
+	sigset_t set;
+	sigset_t old_set;
+	int old_errno;
+	pid_t pid;
+
+	// Empty sigset
+	if (sigemptyset(&set)) {
+		printf("sigtstp handler : sigemptyset failed\n");
+	}
+
+	// Add SIGTSTP to sigset
+	if (sigaddset(&set, SIGTSTP)) {
+		printf("sigtstp handler : sigemptyset failed\n");
+	}
+
+	// Backup errno
+	old_errno = errno;
+
+	// If current foreground job exist
+	if ((pid = fgpid(jobs))) {
+
+		// Block SIGTSTP to prevent unexpected behavior
+		if (sigprocmask(SIG_BLOCK, &set, &old_set)) {
+			printf("sigtstp handler : sigprocmask block SIGTSTP failed\n");
+		}
+
+		// Send SIGTSTP signal to job
+		if (kill(pid, SIGTSTP)) {
+			printf("sigtstp handler : kill SIGTSTP to pid %d failed\n", pid);
+		}
+
+		// Restore sigset prevent unexpected behavior
+		if (sigprocmask(SIG_SETMASK, &old_set, NULL)) {
+			printf("sigtstp handler : sigprocmask restore sigset failed\n");
+		}
+	}
+
+	// Restore errno
+	errno = old_errno;
+
 	return;
 }
 
@@ -554,6 +755,3 @@ void sigquit_handler(int sig) {
 	printf("Terminating after receipt of SIGQUIT signal\n");
 	exit(1);
 }
-
-
-
